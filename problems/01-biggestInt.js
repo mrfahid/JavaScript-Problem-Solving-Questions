3caf7eb1c141de3503bb14f1bfc5d5f2bf032c71
Shah Fahid <shahfahidbaloch@gmail.com>
2024-03-15 11:39:30 +0430

    update readme

---
+++ problems/01-biggestInt.js
@@ -0,0 +1,18 @@
+// problem is that find an biggest integer in an array;
+
+
+// <<<<<<<<<<<<<  ✨ Biggest Int >>>>>>>>>>>>>>
+/**
++ * Finds the biggest integer in the given array.
++ * @param {Array} array - An array of integers
++ * @return {number} The biggest integer in the array
++ */
+// <<<<< Solution >>>>>
+function findBiggestInt(array) {
+  
+}
+
+// <<<<< Example: >>>>>
+let result = findBiggestInt(/* add an array here */)
+console.log(result) 
+
--- solutions/01-biggestInt.js
+++ solutions/01-biggestInt.js
@@ -1,20 +1,28 @@
 // problem is that find an biggest integer in an array;
 
 
+
 // <<<<<<<<<<<<<  ✨ Biggest Int >>>>>>>>>>>>>>
 /**
 + * Finds the biggest integer in the given array.
++ * 
 + * @param {Array} array - An array of integers
 + * @return {number} The biggest integer in the array
 + */
 // <<<<< Solutions >>>>>
 function findBiggestInt(array) {
+  // Sort the array in ascending order
   let sortArray = array.sort((a, b) => a - b);
   // console.log(sortArray);
-  let biggestInt = sortArray[sortArray.length - 1];
-  return biggestInt
+
+  // Find the biggest integer from the sorted array
+  const biggestInt = sortArray[sortArray.length - 1];
+
+  // Return the biggest integer
+  return biggestInt;
 }
 
-let array1 = findBiggestInt([5, 3, 22, 55, 44, 1, 0])
-console.log(array1) 
+// <<<<< Example: >>>>>
+let result = findBiggestInt([5, 3, 22, 55, 44, 1, 0])
+console.log(result) 
 
