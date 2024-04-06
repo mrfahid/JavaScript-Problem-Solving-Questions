4f626ae1e8a2ccf28659505b7cf4bcd7ee26cff2
Shah Fahid <shahfahidbaloch@gmail.com>
2024-04-06 22:48:42 +0500

    add 5 five new problems js

---
+++ problems/Anagram-Check.js
@@ -0,0 +1,7 @@
+// Anagram Check: Check if two strings are anagrams of each other.
+
+
+
+// Test cases
+console.log(isAnagram("listen", "silent")); // Output: true
+console.log(isAnagram("hello", "world")); // Output: false
